f3fa1d74fc9620d0913cfe983ef3d90bb7d69446
Justin Lyon <jml6487@gmail.com>
2019-06-10 09:53:16 -0500

    setup goodies rest api

--- force-app/main/default/lwc/goodieList/goodieList.js
+++ force-app/main/default/lwc/goodieList/goodieList.js
@@ -1,6 +1,18 @@
-import { LightningElement, wire } from 'lwc';
+import { LightningElement, api, wire } from 'lwc'
 import getGoodies from '@salesforce/apex/GoodieListAuraService.getGoodies'
 export default class GoodieList extends LightningElement {
+  @api goodies
+  @api error
+
   @wire(getGoodies, {})
-  goodies
+  wireGoodies ({ error, data }) {
+    this.goodies = []
+    if (data) {
+      const theGoods = JSON.parse(data)
+      this.goodies = theGoods
+
+    } else if (error) {
+      this.error = error
+    }
+  }
 }
