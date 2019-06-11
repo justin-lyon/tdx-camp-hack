ca2efee970f84fcedf2268a8efeba9537a884686
Justin Lyon <jml6487@gmail.com>
2019-06-11 14:22:43 -0500

    EOD WIP productList

--- force-app/main/default/lwc/productList/productList.js
+++ force-app/main/default/lwc/productList/productList.js
@@ -3,9 +3,11 @@ import { getListUi } from 'lightning/uiListApi'
 import PRODUCT from '@salesforce/schema/Product__c'
 import PRODUCT_FAMILY from '@salesforce/schema/Product_Family__c'
 
+const LIST_VIEW_PREFIX = 'Family_'
 export default class ProductList extends LightningElement {
   @api recordId
   @track families
+  @track listViews
   @track products
   @track error
   @track selectedProductId
@@ -13,14 +15,17 @@ export default class ProductList extends LightningElement {
   @wire(getListUi, { objectApiName: PRODUCT_FAMILY, listViewApiName: 'All' })
   handleFamilies({ error, data }) {
     if (data) {
-      console.log('data', JSON.stringify(data.records))
-      console.log('count: ', data.records.records.records.count)
-      this.families = data.records.records.records
+      console.log('data', JSON.parse(JSON.stringify(data.records)))
+      console.log('count: ', data.records.count)
+      const families = data.records.records.map(record => ({ Name: record.fields.Name.value, Id: record.fields.Id.value }))
+      console.log('families', families)
+      this.families = families
     } else if (error) {
       console.error('unhandled error: ', error.message)
     }
   }
   // surface in Product Family record page
-  // get products by family
+  // wire this product family record
+  // get products by family through listview
   // iterate over products to c-product-item
 }
