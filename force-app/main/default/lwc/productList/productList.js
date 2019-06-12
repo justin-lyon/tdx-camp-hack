ef6dd312be285fe5843c6e102987b8d3891781ce
Justin Lyon <jml6487@gmail.com>
2019-06-12 08:40:48 -0500

    load family and product data

--- force-app/main/default/lwc/productList/productList.js
+++ force-app/main/default/lwc/productList/productList.js
@@ -1,29 +1,62 @@
 import { LightningElement, api, track, wire } from 'lwc'
+import { getRecord } from 'lightning/uiRecordApi'
 import { getListUi } from 'lightning/uiListApi'
 import PRODUCT from '@salesforce/schema/Product__c'
-import PRODUCT_FAMILY from '@salesforce/schema/Product_Family__c'
+import PRODUCT_FAMILY_NAME from '@salesforce/schema/Product_Family__c.Name'
 
 const LIST_VIEW_PREFIX = 'Family_'
 export default class ProductList extends LightningElement {
   @api recordId
-  @track families
-  @track listViews
-  @track products
   @track error
+
+  @track products
   @track selectedProductId
 
-  @wire(getListUi, { objectApiName: PRODUCT_FAMILY, listViewApiName: 'All' })
+  @track family
+  @track listViewName
+
+  get familyTitle() {
+    return this.family.Name + ' Products'
+  }
+
+  @wire(getRecord, {
+    recordId: '$recordId',
+    fields: [PRODUCT_FAMILY_NAME]
+  }) handleFamily ({ error, data }) {
+    if (data) {
+      this.family = {
+        Name: data.fields.Name.value,
+        Id: data.id
+      }
+      this.setListViewName()
+
+    } else if (error) {
+      this.error = error
+    }
+  }
+
+  @wire(getListUi, { objectApiName: PRODUCT, listViewApiName: '$listViewName' })
   handleFamilies({ error, data }) {
     if (data) {
-      console.log('data', JSON.parse(JSON.stringify(data.records)))
-      console.log('count: ', data.records.count)
-      const families = data.records.records.map(record => ({ Name: record.fields.Name.value, Id: record.fields.Id.value }))
-      console.log('families', families)
-      this.families = families
+      const products = data.records.records.map(record => {
+        return {
+          Name: record.fields.Name.value,
+          Id: record.fields.Id.value,
+          Category__c: record.fields.Category__c.value,
+          Level__c: record.fields.Level__c.value,
+          MSRP__c: record.fields.MSRP__c.value
+        }
+      })
+      this.products = products
     } else if (error) {
       console.error('unhandled error: ', error.message)
     }
   }
+
+  setListViewName() {
+    this.listViewName = LIST_VIEW_PREFIX + this.family.Name.toUpperCase()
+  }
+
   // surface in Product Family record page
   // wire this product family record
   // get products by family through listview
